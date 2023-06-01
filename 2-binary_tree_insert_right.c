55e2411cc8487c8f38e4b1510f8198e22447014b
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-06-01 09:10:52 +0000

    se

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -20,8 +20,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
     {
       return (NULL);
     }
-  new_node = parent;
   new_node->n = value;
+  new_node->parent = parent;
   new_node->left = NULL;
   new_node->right = parent->right;
   parent->right = new_node;
