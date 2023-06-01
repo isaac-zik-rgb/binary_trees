ed18b098ded83bbd72c81ea78ea781f5d5adef39
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-06-01 08:36:42 +0000

    done

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,10 +12,10 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_node;
- if (parent == NULL)
-   {
-     return (NULL);
-   }
+if (parent == NULL)
+{
+return (NULL);
+}
 
 new_node = malloc(sizeof(binary_tree_t));
 
@@ -28,7 +28,7 @@ new_node->parent = parent;
 new_node->n = value;
 if (parent->left != NULL)
 {
-new_node->left = parent->left;
+parent->left = new_node;
 }
 new_node->left = parent->left;
 parent->left = new_node;/* Set the new node as the left-child */
