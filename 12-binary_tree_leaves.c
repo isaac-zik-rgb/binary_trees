a33d1e3c7de34718252a02fb8d598e126c6ca411
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-06-05 18:49:56 +0000

    task12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - count the number of leaves in a tree
+ * @tree: A pointer to the root node of the tree
+ *
+ * Return: if tree is NULL return 0 else the number of leaves
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+size_t left;
+size_t right;
+if (tree == NULL)
+{
+return (0);
+}
+if (tree->left == NULL && tree->right == NULL)
+{
+return (1);
+}
+left = binary_tree_leaves(tree->left);
+right = binary_tree_leaves(tree->right);
+
+return ((left + right)+1);
+}
