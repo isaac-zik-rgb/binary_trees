29acd0c1c9f029e04440a6773486634cda81783f
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-06-05 09:54:09 +0000

    set

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,8 +9,8 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-  size_t left_height;
-  size_t right_height;
+  size_t left_height = 0;
+  size_t right_height = 0;
   if (tree == NULL)
     {
       return (0);
@@ -20,8 +20,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
   if (left_height > right_height)
     {
-      return (left_height);
+      return (left_height + 1);
     }
   else
-    return (right_height);
+    return (right_height + 1);
 }
