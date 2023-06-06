0c11c8985268f1caf7aa6136ffe761c21886d9ba
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-06-06 20:56:33 +0000

    task14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,7 +9,7 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-if (tree == NULL)
+if (tree)
 return ((binary_tree_balance(tree->left)) - (binary_tree_balance(tree->right)));
 return (0);
 }
